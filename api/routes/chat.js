6a88ecdcbebc8aa643887f72885313b9ce1f1903
Ruchika Chakraborty <ruchika6302@gmail.com>
2023-07-18 01:46:51 +0530

    New buld

--- api/routes/chat.js
+++ api/routes/chat.js
@@ -1,7 +1,7 @@
 import express from "express";
 import User from "../models/User.js"
 import Chat from "../models/Chat.js"
-import { verifyToken } from "../Utils/verifyToken.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
